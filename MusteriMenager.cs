6d14b4cc9de16731e1c204940c9caba8f66490e1
Hatice <77589233+htc18kck@users.noreply.github.com>
2021-01-21 22:18:04 +0300

    Add files via upload

---
+++ MusteriMenager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriMenager
+    {
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri no: " + musteri.Id+ " Adınız: "+musteri.Ad+" Soyadınız: "+musteri.Soyad+" Tc kimlik numaranız: "+musteri.TcNo+" Kaydınız tamamlandı.");
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri no: " + musteri.Id + " Adınız: " + musteri.Ad + " Soyadınız: " + musteri.Soyad + " Tc kimlik numaranız: " + musteri.TcNo + " Kaydınız silindi.");
+
+        }
+        public void MusteriBilgileriGuncelleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri no: " + musteri.Id + " Adınız: " + musteri.Ad + " Soyadınız: " + musteri.Soyad + " Tc kimlik numaranız: " + musteri.TcNo + " Kaydınız güncellendi.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Hatice";
+            musteri1.Soyad = "Koçak";
+            musteri1.TcNo = "12345678910";
+            musteri1.Id = 1;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Zehra";
+            musteri2.Soyad = "Koçak";
+            musteri2.TcNo = "12345695263";
+            musteri2.Id = 2;
+
+            Musteri musteri3= new Musteri();
+            musteri3.Ad = "Fatma";
+            musteri3.Soyad = "Yıldız";
+            musteri3.TcNo = "1236541231223";
+            musteri3.Id = 3;
+
+            MusteriMenager musteriMenager = new MusteriMenager();
+            musteriMenager.MusteriEkleme(musteri1);
+            musteriMenager.MusteriEkleme(musteri2);
+            musteriMenager.MusteriEkleme(musteri3);
+            musteriMenager.MusteriSilme(musteri1);
+            musteriMenager.MusteriSilme(musteri2);
+            musteriMenager.MusteriSilme(musteri3);
+            musteriMenager.MusteriBilgileriGuncelleme(musteri1);
+            musteriMenager.MusteriBilgileriGuncelleme(musteri2);
+            musteriMenager.MusteriBilgileriGuncelleme(musteri3);
+
+
+
+
+        }
+    }
+}
